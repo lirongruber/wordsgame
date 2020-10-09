0fc41895043aad05fa7fc713e9eb0c8182844bef
lirongruber <35901248+lirongruber@users.noreply.github.com>
2020-10-09 20:56:40 +0300

    get back to game
    1 get back to game
    2. share the same game
    3. animation for the winner

--- logic.js
+++ logic.js
@@ -61,15 +61,87 @@ render = function (state) {
     })
     $(".control").addClass(state.teams_order[0]);
 }
+
+winner_interval = 0;
+render_winner = function(winner, stop){
+    $('.winning').removeClass('is-hidden')
+    new_state['running'] = false;
+    winner_interval= setInterval(function(){ 
+        $('.winning').removeClass('is-hidden')        
+        $('.winning').solitaireVictory();
+    }, 100);
+    
+}
+
+stop_render_winner = function(){
+    $('.winning').addClass('is-hidden')
+    
+    clearInterval(winner_interval)
+    $('').solitaireVictory({clear: true});
+    
+    winner_interval = "stopped"
+}
+
+
 listener = function () {
+    $(document).on("click",".winning", function() 
+    {
+        stop_render_winner()
+        
+        
+        
+        
+    });
+    $("#new_game").on("click",function(e){
+        if (!confirm("Do you want to create a new game?")){
+            return;
+        }
+
+        let searchParams = new URLSearchParams(window.location.search)
+        searchParams.set("gameId", ""); // setting your param
+        history.pushState(null, null, "?"+searchParams.toString());        
+        location.reload(true);
+        
+    })
     $("#board").on("click", "*", function (e) {
+
+        
         $(this).toggleClass($(this).data('color'));
         $(this).data('clicked', $(this).hasClass($(this).data('color')));
         console.log($(this).data('id'))
         console.log($(this).data('color'))
+        visible_red_cards = $("#board .card.red")
+        visible_blue_cards = $("#board .card.blue")
+
+        if   (winner_interval == "stopped"){
+            return;
+        }
+        
+        if (first_team == $("#board .card."  + new_state["teams_order"][0]).length){
+            //first_team winns
+            console.log(teams_order[0] + " wins")
+            $('.winning').addClass(teams_order[0])
+            render_winner()
+        }
+
+        if (second_team == $("#board .card."  + new_state["teams_order"][1]).length){
+            console.log(teams_order[1] + " wins")
+            $('.winning').addClass(teams_order[1])
+            render_winner()
+            
+        }
+   
+       
+
     })
     $("#is_manager").on("click", function (e) {
         is_manager = $(this).is(':checked')
+        if (is_manager){
+            if (!confirm("Do you want to reveal the board?")){
+                $(this).prop('checked', false);
+                return;
+            }
+        }
         $("#board").toggleClass("is-manager");
         $('.card').each(function (card, i, lst) {
             if (is_manager) {
